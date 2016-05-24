d17bb7581ac23a24bcff85841768c284676c844b
Brandon Myers <bdmyers@cs.washington.edu>
2016-05-24 21:55:05 +0000

    use with python 3 and HTML

--- fetchmovies.py
+++ fetchmovies.py
@@ -1,11 +1,13 @@
 from bs4 import BeautifulSoup
-from urllib2 import urlopen, HTTPError, URLError
-from urlparse import urlparse, urljoin
+from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
+from urllib.parse import urlparse, urljoin
 import os.path, errno
 import shutil
-import subprocess32 as subprocess
+import subprocess as subprocess
 from concurrent.futures import *
 from itertools import islice
+import ssl
 
 # Crawl a Thredds server and generate a list of all movie files
 
@@ -14,26 +16,22 @@ def domainof(url):
   domain = '{uri.scheme}://{uri.netloc}'.format(uri=parts)
   return domain
 
-def moviecrawl(url):
-  # Walk a Thredds directory and generate a list of all .mov and .mp4 files
+def moviecrawl_html(url):
+  '''
+      This function crawls throught the html at the url,
+      and extracts text fields containing '.mp4',
+      thus generating a list of the filenames in this dir.
+  '''
+  
   domain = domainof(url)
-  response = urlopen(url)
-  soup = BeautifulSoup(response.read(), "html.parser") 
-
-  # yield all movie urls on this page, if any
-  for ds in soup.find_all("thredds:dataset"):
-    movurl = domain + ds[u'id']
-    if "mov" in movurl:
-        yield movurl
-    if "mp4" in movurl:
-        yield movurl
-
-  for ds in soup.find_all("thredds:dataset"):
-    base = ds[u'id']
-    for c in ds.find_all("thredds:catalogref"):
-      newurl = urljoin(urljoin(domain,base), c[u'xlink:href'])
-      for movieurl in moviecrawl(newurl):
-        yield movieurl
+  response = urlopen(url,context = ssl._create_unverified_context())
+  soup = BeautifulSoup(response.read(), "html.parser")
+  
+  # create a list .mp4 filenames in this directory
+  movienames = []
+  for item in soup.find_all(text=True):
+    if 'mp4' in item:
+      yield url+'/'+item
 
 def getlocalpath(url, localroot="."):
     # Return the local path derived from the url
@@ -52,7 +50,7 @@ def getlocalpath(url, localroot="."):
 def download(url, local_file_name):
     # download url to local_File_name, return true if successful
     try:
-        response = urlopen(url)
+        response = urlopen(url,context = ssl._create_unverified_context())
 
         CHUNK = 16 * 1024
         with open(local_file_name, 'wb') as f:
@@ -64,10 +62,10 @@ def download(url, local_file_name):
 
         return True
     #handle errors
-    except HTTPError, e:
-        print "HTTP Error:",e.code , url
-    except URLError, e:
-        print "URL Error:",e.reason , url
+    except HTTPError as e:
+        print("HTTP Error:",e.code , url)
+    except URLError as e:
+        print("URL Error:",e.reason , url)
 
     return False
 
@@ -107,16 +105,16 @@ def fetch(url, localroot="."):
   localfile = getlocalpath(url, localroot)
   gname = getgooglename(localfile)
   if not googleexists(gname):
-    print "downloading ", url
+    print("downloading ", url)
     downloaded = download(url, localfile)
     if downloaded:
-      print "uploading", localfile 
+      print("uploading", localfile) 
       uploadtoGoogle(localfile, gname)
-      print "cleaning up"
+      print("cleaning up")
       delete(localfile)
       return googlename
   else:
-    print "....already uploaded ", localfile
+    print("....already uploaded ", localfile)
       
 
 def batched_executor(f, iterable, pool, batch_size):
@@ -133,17 +131,18 @@ def transload(rooturl):
   # Crawl the ooi OPeNDAP pages, download files, move to Google drive
   N = 5
   with ThreadPoolExecutor(max_workers=N) as pool:
-    for result in batched_executor(fetch, moviecrawl(url), pool, N):
+    for result in batched_executor(fetch, moviecrawl_html(url), pool, N):
       yield result
 
 def test():
   url = "http://opendap-devel.ooi.rutgers.edu:8080/opendap/hyrax/large_format/RS03ASHS-PN03B-06-CAMHDA301/2016/02/10/CAMHDA301-20160210T180000Z.mp4"
   url = "http://opendap-devel.ooi.rutgers.edu:8080/opendap/hyrax/large_format/RS03ASHS-PN03B-06-CAMHDA301/2016/catalog.xml"
-  for url in moviecrawl(url):
+  url = "https://rawdata.oceanobservatories.org/files/RS03ASHS/PN03B/06-CAMHDA301/2016/04/04/" 
+  for url in moviecrawl_html(url):
     local_file_name = getlocalpath(url)
     if not os.path.exists(local_file_name):
-      download(url)
-    uploadtoGoogle(local_file_name)
+      download(url, local_file_name)
+#    uploadtoGoogle(local_file_name)
   #print googleexists("opendap_hyrax_large_format_RS03ASHS-PN03B-06-CAMHDA301_2016_01_01_CAMHDA301-20160101T000000Z.mp4")
 
 if __name__ == '__main__': 
